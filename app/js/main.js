4beac5d3273c39c69f1ebe7c4ef5d1b1eeac4abd
Danil1906 <Danilkozlov36@gmail.com>
2022-05-24 06:22:46 +0300

    mane added

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
     $('.product-benefits__slider-wrapper').slick({
-
+        
+        nextArrow: '<button type="button" class="custom_arrow custom_arrow-next"></button>',
+        prevArrow: '<button type="button" class="custom_arrow custom_arrow-prev"></button>',
+        dots: true,
       });
 })
