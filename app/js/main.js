f4b89dac3f3f0b5c95d1d26d0acf0026cd398244
Danil1906 <Danilkozlov36@gmail.com>
2022-05-24 23:55:44 +0300

    start monile adapty

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,15 @@
-$(function(){
+$(function () {
     $('.product-benefits__slider-wrapper').slick({
-        
+
         nextArrow: '<button type="button" class="custom_arrow custom_arrow-next"></button>',
         prevArrow: '<button type="button" class="custom_arrow custom_arrow-prev"></button>',
         dots: true,
-      });
+    });
+
+    $('a[href^="#"]').click(function () { // #1
+        let anchor = $(this).attr('href');  // #2
+        $('html, body').animate({           // #3
+            scrollTop: $(anchor).offset().top  // #4
+        }, 600);                            // #5
+    });
 })
